60be007ed3ecc6db06bb8861ad4817c311793114
redesycomponentesMluisa <marialuisa@redesycomponentes.com>
2024-05-27 12:46:41 +0200

    Modificada apariencia de categorias en index

--- resources/views/sections/index.blade.php
+++ resources/views/sections/index.blade.php
@@ -60,27 +60,38 @@
             </div>
         </div>
         <!-- categorias disponibles -->
-        <div class="favoritos">
-            <button id="scrollLeft" class="btn btn-link"><i
-                    class="bi bi-arrow-left-circle-fill font-24 text-primary"></i></button>
-            <div id="productos" class="productos gap-3">
-                @foreach($categories as $category)
-                <div class="col d-flex flex-column align-content-between  align-items-center">
-                    <a href="{{ route('categories', ['catcod' => $category->id]) }}" title="">
-                        <img src="{{ asset('images/categorias/' . $category->imagen) }}" class="img-fluid"
-                            alt="{{ $category->name }}" style="width:121px;height:121px;"
-                            onerror="this.onerror=null; this.src='{{ asset('images/articulos/noimage.jpg') }}';">
-                    </a>
-                    <h3 class="text-center">
-                        <a href="{{ route('categories', ['catcod' => $category->id]) }}">{{ $category->nombre_es }}</a>
-                    </h3>
+        <div class="row justify-content-center">
+            <div class="favoritos">
+                <button id="scrollLeft" class="btn btn-link"><i
+                        class="bi bi-arrow-left-circle-fill font-24 text-primary"></i></button>
+                <div id="productos" class="productos gap-3">
+                    @foreach($categories as $category)
+                    <div class="categoria col-2 d-block position-relative p-0 m-2">
+                        <a href="{{ route('categories', ['catcod' => $category->id]) }}" title=""
+                            onclick="irAProductos()">
+                            <img src="{{ asset('images/categorias/' . $category->imagen) }}"
+                                class="object-fit-fill border rounded" alt="{{ $category->nombre_es }}"
+                                style="height:200px; width:100%;"
+                                onerror="this.onerror=null; this.src='{{ asset('images/articulos/noimage.jpg') }}';">
+                        </a>
+                        <div class="nombre-categoria bg-primary text-center">
+                            <h5>
+                                <a href="{{route('categories', ['catcod' => $category->id])}}" class="text-white"
+                                    onclick="irAProductos()">
+                                    {{ $category->nombre_es }}
+                                </a>
+                            </h5>
+                            <a href="{{route('categories', ['catcod' => $category->id])}}" onclick="irAProductos()"
+                                class="categoria-link text-warning font-20">Ver más <i
+                                    class="bi bi-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
-                @endforeach
+                <button id="scrollRight" class="btn btn-link"><i
+                        class="bi bi-arrow-right-circle-fill font-24 text-primary"></i></button>
             </div>
-            <button id="scrollRight" class="btn btn-link"><i
-                    class="bi bi-arrow-right-circle-fill font-24 text-primary"></i></button>
         </div>
-        <!--fin categorias disponibles -->
 
 
         <!-- historico -->
