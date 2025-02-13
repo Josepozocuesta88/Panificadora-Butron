21b0fa8d93ebea4c723c9e410aed47e4911c2baa
Arturo-Grandson <arturo.nietomuriel@gmail.com>
2025-02-13 10:20:34 +0100

    corregido un error que impedia entrar en mis pedidos

--- resources/views/pages/ecommerce/pedidos/estado.blade.php
+++ resources/views/pages/ecommerce/pedidos/estado.blade.php
@@ -163,7 +163,6 @@
                     $items->reduce(function ($carry, $item) {
                     $total = round($item['precio'] * $item['cantidad'], 2);
                     // Auth::user()->usuivacod == "S" ? $carry += $total * ($item['recargo_porcentaje'] / 100) : $carry
-                    =
                     // 0;
                     Auth::user()->usuivacod != "N" && Auth::user()->usuivacod != "E" ? $carry += $total *
                     ($item['recargo_porcentaje'] / 100) : $carry = 0;
