5a16284e11956ece2e3ba9b2406233234f921037
Jorge <jluiscontreras95@gmail.com>
2025-06-20 13:41:04 +0200

    Enhance oferta retrieval logic to include articles by family code; refactor user code variable for consistency

--- app/Services/OfertasPersonalizadasService.php
+++ app/Services/OfertasPersonalizadasService.php
@@ -9,16 +9,59 @@ use App\Models\OfertaC;
 
 class OfertasPersonalizadasService implements OfertaServiceInterface
 {
+  // public function obtenerOfertas()
+  // {
+  //   $today      = Carbon::now();
+  //   $usuofecod  = Auth::user()->usuofecod;
+
+  //   $ofertas    = OfertaC::with('articulo')->whereDate('ofcfecfin', '>=', $today)
+  //     ->where(function ($query) use ($usuofecod) {
+  //       $query->where('ofccod', $usuofecod);
+  //     })
+  //     ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuofecod])
+  //     ->get();
+
+  //   foreach ($ofertas as $oferta) {
+  //     if ($oferta->ofcima === null || $oferta->ofcima === '') {
+  //       $oferta->ofcima = "noimage.jpg";
+  //     }
+  //   }
+  //   return $ofertas;
+  // }
+
+  // public function obtenerArticulosEnOferta()
+  // {
+  //   $today      = Carbon::now();
+  //   $user       = Auth::user();
+  //   $usuofecod  = $user ? $user->usuofecod : null;
+  //   $articulos  = OfertaC::with('articulo')->whereDate('ofcfecfin', '>=', $today)
+  //     ->where('ofccod', $usuofecod)
+  //     ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuofecod])
+  //     ->get()
+  //     ->pluck('articulo')
+  //     ->filter(function ($articulo) {
+  //       return $articulo !== null;
+  //     });
+
+  //   return $articulos;
+  // }
+
+
+  // prueba
   public function obtenerOfertas()
   {
     $today      = Carbon::now();
-    $usuofecod  = Auth::user()->usuofecod;
+    $usuclicod  = Auth::user()->usuclicod;
 
-    $ofertas    = OfertaC::with('articulo')->whereDate('ofcfecfin', '>=', $today)
-      ->where(function ($query) use ($usuofecod) {
-        $query->where('ofccod', $usuofecod);
+    $ofertas    = OfertaC::with('articulo')
+      ->whereDate('ofcfecfin', '>=', $today)
+      ->where(function ($query) use ($usuclicod) {
+        $query->where('ofccod', $usuclicod)
+          ->orWhereHas('articulo', function ($q) use ($usuclicod) {
+            $q->where('artfamcod', $usuclicod);
+          });
       })
-      ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuofecod])
+      ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuclicod])
       ->get();
 
     foreach ($ofertas as $oferta) {
@@ -33,10 +76,17 @@ class OfertasPersonalizadasService implements OfertaServiceInterface
   {
     $today      = Carbon::now();
     $user       = Auth::user();
-    $usuofecod  = $user ? $user->usuofecod : null;
-    $articulos  = OfertaC::with('articulo')->whereDate('ofcfecfin', '>=', $today)
-      ->where('ofccod', $usuofecod)
-      ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuofecod])
+    $usuclicod  = $user ? $user->usuclicod : null;
+
+    $articulos  = OfertaC::with('articulo')
+      ->whereDate('ofcfecfin', '>=', $today)
+      ->where(function ($query) use ($usuclicod) {
+        $query->where('ofccod', $usuclicod)
+          ->orWhereHas('articulo', function ($q) use ($usuclicod) {
+            $q->where('artfamcod', $usuclicod);
+          });
+      })
+      ->orderByRaw("CASE WHEN ofccod = ? THEN 1 ELSE 2 END", [$usuclicod])
       ->get()
       ->pluck('articulo')
       ->filter(function ($articulo) {
