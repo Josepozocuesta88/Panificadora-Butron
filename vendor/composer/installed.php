956b09fb98c0baf3372a1ed259a6909161ef14e7
Arturo-Grandson <arturo.nietomuriel@gmail.com>
2025-06-30 08:17:12 +0200

    composer install

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9f9bdc6dde67afbe0c0ddadc4a22c71857d19cd2',
+        'reference' => 'd6ed342f509a96253ba534f640c3f5040a15ff3a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9f9bdc6dde67afbe0c0ddadc4a22c71857d19cd2',
+            'reference' => 'd6ed342f509a96253ba534f640c3f5040a15ff3a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -784,8 +784,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
