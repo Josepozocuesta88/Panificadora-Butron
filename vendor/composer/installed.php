45018705071a300ac2cfcd231be014f88f67c2f2
Jorge Contreras <jluiscontreras95@gmail.com>
2025-10-07 15:17:11 +0000

    se regenera el vendor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'eb67f6a31df6e1e61762b46f0af746c2be16d682',
+        'reference' => 'b8c978d6f137628225def531d019f1a15929d8ca',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'eb67f6a31df6e1e61762b46f0af746c2be16d682',
+            'reference' => 'b8c978d6f137628225def531d019f1a15929d8ca',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -784,8 +784,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
